d6bce409d11d299c24fb6861c8d4016783d2e45f
rajanand02 <rajanand0202@gmail.com>
2014-03-24 15:58:04 +0530

    add graphs page layout

--- client/main.js
+++ client/main.js
@@ -4,3 +4,11 @@ Template.grapher.logged_in = function () {
 Template.grapher.graph_selected = function () {
   return Session.get("current_graph");
 };
+Template.graphs_page.has_graphs = function () {
+  return GraphsModel.find({}).count();
+}
+Template.graphs_page.graphs = function () {
+  return GraphsModel.find({});
+}
+  
+
