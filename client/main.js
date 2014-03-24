b359ecdf18358030ec1e93019dddc9deecea6923
rajanand02 <rajanand0202@gmail.com>
2014-03-24 19:46:09 +0530

    include node template and related css

--- client/main.js
+++ client/main.js
@@ -10,5 +10,22 @@ Template.graphs_page.has_graphs = function () {
 Template.graphs_page.graphs = function () {
   return GraphsModel.find({});
 }
-  
-
+Template.graphs_page.events({
+  "click #new_graph" : function() {
+    Grapher.Graphs.create("New Graph (double click to rename)", Meteor.userId());
+  },
+  "dblclick .graph_title" : function() {
+    var new_name = prompt("Please enter the new name", this.name);
+    if(new_name) {
+      Grapher.Graphs.rename(this._id, new_name);
+    }
+  },
+  "click .delete_graph" : function() {
+    if(confirm("Are you sure you want to remove '" + this.name + "'")) {
+      Grapher.Graphs.delete(this._id);
+    }
+  },
+  "click .open_graph" : function() {
+    Grapher.Graphs.change_current(this);
+  }
+});
